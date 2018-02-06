530b7833d5baf558b1f06195b60349bfd084c244
Alex Wilson <a@ax.gy>
2018-02-06 15:29:43 +0000

    bump

--- front-end/src/client.js
+++ front-end/src/client.js
@@ -6,7 +6,7 @@ web3.setProvider(new Web3.providers.HttpProvider("/__service/parity/v1"))
 
 window.web3 = web3
 
-const knownParityUser = "0xa656705d507cbce676a709cd1a245db050ca4b98"
+const knownParityUser = "0x0280c211316ce2c1c76dcc91a6de336c60d18d31"
 const knownParityPassword = "test1234"
 const knownTransaction = '0x0ab2c90b6c3b5d08ab0269910f74baa893674941'
 const GreenGoodGuyGasABI = [{"constant":true,"inputs":[],"name":"getDecaySinceGeneration","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"locationType","type":"string"}],"name":"lockContract","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"locationType","type":"string"},{"name":"owner","type":"string"}],"name":"newValueInTime","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"calcDecay","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"getValuesInTime","outputs":[{"components":[{"name":"value","type":"uint256"},{"name":"locationType","type":"string"},{"name":"owner","type":"string"}],"name":"","type":"tuple[]"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"locationType","type":"string"},{"name":"owner","type":"string"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]
